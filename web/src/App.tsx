f9ca4db702885933a4b99cd750fd1cca6505856a
Sean Rennie <rnnsea001@gmail.com>
2023-02-06 06:29:27 +0000

    ad some controls

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,28 @@
 import './index.css';
-import { Canvas, ThreeElements, useFrame } from '@react-three/fiber';
-import { useRef, useState } from 'react';
-import { Mesh } from 'three';
 
-function Box(props: ThreeElements['mesh']) {
+import { Canvas, ThreeElements, useFrame } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
+import { DoubleSide, Mesh } from 'three';
+import { CameraControls } from '@react-three/drei';
+
+function HalfSphere(props: ThreeElements['mesh']) {
   // This reference will give us direct access to the mesh
   const mesh = useRef<Mesh>(null!);
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
+  useEffect(() => {
+    mesh.current.rotation.x += (Math.PI / 2) * 3;
+  }, []);
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => {
-    // mesh.current.rotation.x += delta;
-    mesh.current.rotation.y -= delta / 4;
-  });
+  // useFrame((state, delta) => {
+  //   // mesh.current.rotation.x += delta;
+  //   mesh.current.rotation.y -= delta / 4;
+  // });
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
       {...props}
       ref={mesh}
-      // scale={active ? 1.5 : 1}
-
-      onClick={(event) => {
-        mesh.current.rotation.x += Math.PI / 2;
-        return setActive(!active);
-      }}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
       position={[0, 0, 0]}
@@ -35,13 +33,40 @@ function Box(props: ThreeElements['mesh']) {
   );
 }
 
+function Plane(props: ThreeElements['mesh']) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef<Mesh>(null!);
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  // useFrame((state, delta) => {
+  //   // mesh.current.rotation.x += delta;
+  //   mesh.current.rotation.y -= delta / 4;
+  // });
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+      position={[0, 0, -2]}
+    >
+      <planeGeometry args={[9, 9]} />
+      <meshBasicMaterial color={'red'} side={DoubleSide} />
+    </mesh>
+  );
+}
+
 export function App() {
   return (
     <div id="canvas-container">
       <ambientLight args={[0xff0000]} intensity={0.1} />
       <directionalLight position={[0, 0, 5]} intensity={0.5} />
       <Canvas>
-        <Box />
+        <CameraControls />
+        <HalfSphere />
+        <Plane />
       </Canvas>
     </div>
   );
