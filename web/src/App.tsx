5ec13b45c83ecfe3a77c405dbf2495f923d5f92c
Sean Rennie <rnnsea001@gmail.com>
2023-03-11 17:09:00 +0000

    stuff

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,69 +1,50 @@
 import './index.css';
 
-import { Canvas, ThreeElements, useFrame } from '@react-three/fiber';
-import { useEffect, useRef, useState } from 'react';
-import { DoubleSide, Mesh } from 'three';
 import { CameraControls } from '@react-three/drei';
+import { Canvas, ThreeElements, useFrame, useThree } from '@react-three/fiber';
+import { useEffect, useRef, useState } from 'react';
+import { DoubleSide, Mesh, Vector3 } from 'three';
+
+const WIDTH = 4;
+const HEIGHT = WIDTH / 2;
 
 function HalfSphere(props: ThreeElements['mesh']) {
-  // This reference will give us direct access to the mesh
   const mesh = useRef<Mesh>(null!);
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  useEffect(() => {
-    mesh.current.rotation.x += (Math.PI / 2) * 3;
-  }, []);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  // useFrame((state, delta) => {
-  //   // mesh.current.rotation.x += delta;
-  //   mesh.current.rotation.y -= delta / 4;
-  // });
-  // Return view, these are regular three.js elements expressed in JSX
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-      position={[0, 0, 0]}
-    >
-      <sphereGeometry args={[3, 32, 16, 0, Math.PI * 2, 0, Math.PI / 2]} />
-      <meshBasicMaterial wireframe={true} color={'grey'} />
+    <mesh {...props} ref={mesh} position={[0, 0, 0]}>
+      <sphereGeometry args={[HEIGHT, 32, 32, 0, Math.PI * 2, Math.PI / 2, Math.PI / 2]} />
+      <meshStandardMaterial color={'grey'} side={DoubleSide} />
     </mesh>
   );
 }
 
 function Plane(props: ThreeElements['mesh']) {
-  // This reference will give us direct access to the mesh
   const mesh = useRef<Mesh>(null!);
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  // useFrame((state, delta) => {
-  //   // mesh.current.rotation.x += delta;
-  //   mesh.current.rotation.y -= delta / 4;
-  // });
-  // Return view, these are regular three.js elements expressed in JSX
+  useEffect(() => {
+    mesh.current.rotation.x = Math.PI;
+  }, []);
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-      position={[0, 0, -2]}
-    >
-      <planeGeometry args={[9, 9]} />
-      <meshBasicMaterial color={'red'} side={DoubleSide} />
+    <mesh {...props} ref={mesh} position={[0, -HEIGHT / 2, 0]} castShadow={true}>
+      <planeGeometry args={[WIDTH, HEIGHT]} />
+      <meshStandardMaterial color={'red'} side={DoubleSide} />
     </mesh>
   );
+}
+
+function CameraState() {
+  const camera = useThree((state) => state.camera);
+  camera.position.set(0, 5, 0);
+  camera.lookAt(0, 0, 0);
+  return null;
 }
 
 export function App() {
   return (
     <div id="canvas-container">
-      <ambientLight args={[0xff0000]} intensity={0.1} />
-      <directionalLight position={[0, 0, 5]} intensity={0.5} />
-      <Canvas>
+      <Canvas shadows={true}>
+        <ambientLight args={[0xff0000]} intensity={0.1} />
+        <pointLight position={[0, 10, -5]} intensity={0.5} />
+        <CameraState />
         <CameraControls />
         <HalfSphere />
         <Plane />
